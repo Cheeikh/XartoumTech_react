ca3a51d56189fdbb6c8330e7a522b1b430f59fb2
Cheeikh <cheeikhmbacke@gmail.com>
2025-02-07 21:19:30 +0000

    feat : heroku to render

--- src/axios.js
+++ src/axios.js
@@ -5,7 +5,7 @@ import { toast } from "react-toastify";
 import { store } from "./redux/store";
 
 export const makeRequest = axios.create({
-    baseURL: "https://xartoom-tech-d55dace11841.herokuapp.com" // Assurez-vous que c'est l'URL correcte de votre backend
+    baseURL: "https://xartoum-tech-react-back.onrender.com" // Assurez-vous que c'est l'URL correcte de votre backend
 });
 
 export const getToken = () => {
